42d720045097faa0da3dbfe8b99a8dc5edbe0e23
CORRADO VECCHIO s270340 <s270340@studenti.polito.it>
2020-09-28 12:36:32 +0200

    0.0.0.1

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
+#include "FileWatcher.h"
+#include <filesystem>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include <boost/thread.hpp>
+
+namespace fs = std::filesystem;
+
+int main(int argc, char* argv[]) {
+    if(argc < 2){
+        std::cerr << "Error: parameters missing";
+        return -1;
+    }
+    fs::path folder = argv[1];
+    std::cout << "Syncronizing folder " << folder << std::endl;
+
+
+
+
+    // Create a FileWatcher instance that will check the current folder for changes every 5 seconds
+    FileWatcher fw{argv[1], std::chrono::milliseconds(5000)};
+
+    // Start monitoring a folder for changes and (in case of changes)
+    // run a user provided lambda function
+    fw.start([folder] (std::string path_to_watch, FileStatus status) -> void {
+        // Process only regular files, all other file types are ignored
+        if(!fs::is_regular_file(fs::path(path_to_watch)) && status != FileStatus::erased) {
+            return;
+        }
+
+        switch(status) {
+            case FileStatus::created:
+                std::cout << "File created: " << path_to_watch << std::endl;
+                std::cout << "path relativo " << fs::relative(path_to_watch, folder) << std::endl;
+                break;
+            case FileStatus::modified:
+                std::cout << "File modified: " << path_to_watch << std::endl;
+                std::cout << "path relativo " << fs::relative(path_to_watch, folder) << std::endl;
+                break;
+            case FileStatus::erased:
+                std::cout << "File erased: " << path_to_watch << std::endl;
+                std::cout << "path relativo " << fs::relative(path_to_watch, folder) << std::endl;
+                break;
+            default:
+                std::cout << "Error! Unknown file status.\n";
+        }
+    });
 }
