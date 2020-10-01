7a3e68ed2c6565378e5d92c0fd15bcca21d417c8
GianMol <gianluca.moliteo@hotmail.com>
2020-10-01 13:27:30 +0200

    0.1.3

--- Client/main.cpp
+++ Client/main.cpp
@@ -3,13 +3,35 @@
 #include <filesystem>
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <map>
+#include <openssl/evp.h>
+#include <string>
 
-#include <boost/algorithm/string.hpp>
 
 namespace fs = std::filesystem;
 #define SIZE 1024
 
-std::string translate_path_to_cyg(fs::path path){
+enum operation { create, del, append };
+enum type { folder, file, file_empty };
+
+struct protocol{
+    fs::path path;
+    operation op;
+    type type;
+    std::string content;
+};
+
+struct sync_protocol{
+    std::map<fs::path, std::string> client_paths;
+    std::vector<fs::path> modified_paths;
+};
+
+/***************** PROTOTYPES ***********************/
+int send_file(fs::path& path, operation op = create);
+
+
+std::string translate_path_to_cyg(fs::path& path){
     #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__) || defined(__CYGWIN__)
         std::string fol = path.u8string();
         if (fol[1] == ':') { //if path is a windows absolute path
@@ -27,7 +49,7 @@ std::string translate_path_to_cyg(fs::path path){
     return path;
 }
 
-std::string translate_path_to_win(fs::path path){
+std::string translate_path_to_win(fs::path& path){
     #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__) || defined(__CYGWIN__)
         std::string fol = path.u8string().substr(10);
         std::replace(fol.begin(), fol.end(), '/', '\\');
@@ -45,41 +67,148 @@ std::string translate_path_to_win(fs::path path){
     return path;
 }
 
-int send_file(fs::path& path){
+int compute_hash(fs::path& path, std::string& hash){
+    EVP_MD_CTX *ctx;
+    unsigned char md_value[EVP_MAX_MD_SIZE];
+    unsigned char buf[SIZE];
+    int len;
+
+    ctx = EVP_MD_CTX_new();
+    EVP_MD_CTX_init(ctx);
+    EVP_DigestInit(ctx, EVP_md5());
+
+
+    std::ifstream in;
+    in.open(path, std::ios::binary);
+    if(!in.is_open()){
+        return 0;
+    }
+
+    while(!in.eof()) {
+        in.read((char *) buf, SIZE);
+        if (in.bad()) return 0;
+        else{
+            EVP_DigestUpdate(ctx, buf, SIZE);
+        }
+    }
+
+    EVP_DigestFinal_ex(ctx, md_value, reinterpret_cast<unsigned int *>(&len));
+
+    EVP_MD_CTX_free(ctx);
+    hash = reinterpret_cast< char const* >(md_value);
+    return 1;
+}
+
+int sync(fs::path& folder){
+
+
+    std::map<fs::path, std::string> all_paths;
+
+    for(auto &file : fs::recursive_directory_iterator(folder)) {
+        if(fs::is_directory(file)){
+            all_paths.insert(std::pair<fs::path, std::string>(file, "0"));
+        }
+        else{
+            std::string hash;
+            if(!compute_hash((fs::path &) file, hash)){
+                std::cerr << "Error" << std::endl;
+                return 0;
+            }
+            else {
+                all_paths.insert(std::pair<fs::path, std::string>(file, hash));
+            }
+        }
+    }
+    struct sync_protocol packet;
+    packet.client_paths = all_paths;
+
+
+    for(auto& item : packet.client_paths){
+        std::cout << "first: " << item.first << std::endl;
+        std::cout << "second: " << item.second << std::endl;
+    }
+
+
+    /*
+     *
+     * send to server packet;
+     * the server send to me the modified paths in a vector
+     *
+     * */
+    std::cout << std::endl << "Sono il server. Mappa ricevuta: mando il vettore" << std::endl << std::endl;
+    packet.modified_paths.emplace_back("/cygdrive/c/Users/gianl/Desktop/prova/Nuova cartella/documento.txt");
+    packet.modified_paths.emplace_back("/cygdrive/c/Users/gianl/Desktop/prova/prova2.txt");
+    for(auto &file : packet.modified_paths) {
+        if(!send_file(file)){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int send_file(fs::path& path, operation op){
     std::cout << "leggendo il file: " << path << std::endl;
     if(fs::is_directory(path)){
+        if(op == del){
+            /*
+             *
+             * delete all subfiles and subdirectories
+             * recursive call send_file();
+             *
+             * for(auto& file : fs::recursive_directory_iterator(path)){
+             *      send_file(file, del);
+             * }
+             *
+             *
+             * then, send to server the information about the deletion of the folder
+             *
+             * */
+            return 1;
+        }
         /*
          * send path to server
          *
          * */
         return 1;
     }
-    std::ifstream in;
-    in.open(path, std::ios::binary);
-    if(!in.is_open()){
-        return 0;
+    else if(op == del){
+        /*
+         *
+         * send to server the information about the deletion of the file
+         *
+         *
+         *
+         * */
+        return 1;
     }
-    int i = 0;
-    void *buf = (void *) malloc(SIZE);
-    while(!in.eof()) {
-        in.read((char *) buf, SIZE);
-        if(in.bad()) return 0;
+    else {
+        std::ifstream in;
+        in.open(path, std::ios::binary);
+        if (!in.is_open()) {
+            return 0;
+        }
+        int i = 0;
+        void *buf = (void *) malloc(SIZE);
+        while (!in.eof()) {
+            in.read((char *) buf, SIZE);
+            if (in.bad()) return 0;
 
-        /*  send to server
-         *  if i == 0, then server will create a new file;
-         *  else server will append the existing file;
-         *  we send to the server 00 (create) for i == 0; 01 (append) for i != 0;
-         *  in any case, in the message there will be the content of the file
-         *
-         *
-         */
-        //std::cout << (char*)buf << std::endl;
-        std::cout << i << std::endl;
-        i++;
+            /*  send to server
+             *  if i == 0, then server will create a new file;
+             *  else server will append the existing file;
+             *  we send to the server 00 (create) for i == 0; 01 (append) for i != 0;
+             *  in any case, in the message there will be the content of the file
+             *
+             *
+             */
+            //std::cout << (char*)buf << std::endl;
+            std::cout << i << std::endl;
+            i++;
+        }
+        in.close();
+        free(buf);
+        return 1;
     }
-    in.close();
-    free(buf);
-    return 1;
 }
 
 
@@ -102,14 +231,13 @@ int main(int argc, char* argv[]) {
     // Create a FileWatcher instance that will check the current folder for changes every 5 seconds
     FileWatcher fw{folder, std::chrono::milliseconds(5000)};
 
-
-
-    for(auto &file : fs::recursive_directory_iterator(folder)) {
-        if(!send_file((fs::path &) file)){
-            std::cerr << "Error" << std::endl;
-            return -1;
-        }
+    if(!sync(folder)){
+        std::cerr << "Error sync" << std::endl;
+        return -1;
     }
+
+
+
 
 
 
@@ -125,15 +253,15 @@ int main(int argc, char* argv[]) {
 
         switch(status) {
             case FileStatus::created:
-                std::cout << "File created: " << translate_path_to_win(path_to_watch) << std::endl;
+                std::cout << "File created: " << translate_path_to_win((fs::path&)path_to_watch) << std::endl;
                 std::cout << "path relativo " << fs::relative(path_to_watch, folder) << std::endl;
                 break;
             case FileStatus::modified:
-                std::cout << "File modified: " << translate_path_to_win(path_to_watch) << std::endl;
+                std::cout << "File modified: " << translate_path_to_win((fs::path&)path_to_watch) << std::endl;
                 std::cout << "path relativo " << fs::relative(path_to_watch, folder) << std::endl;
                 break;
             case FileStatus::erased:
-                std::cout << "File erased: " << translate_path_to_win(path_to_watch) << std::endl;
+                std::cout << "File erased: " << translate_path_to_win((fs::path&)path_to_watch) << std::endl;
                 std::cout << "path relativo " << fs::relative(path_to_watch, folder) << std::endl;
                 break;
             default:
